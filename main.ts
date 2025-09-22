adbb12e2186a91fea970b413e928f46f21c64c9b
Masterkoderen <sm@bacha.dk>
2025-09-22 16:58:37 +0000

    Opdater main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+radio.setGroup(1)
+basic.showIcon(IconNames.Yes)
+basic.pause(1000)
+basic.clearScreen()
 basic.forever(function () {
-	
+    radio.sendNumber(input.acceleration(Dimension.Strength))
+    led.toggle(0, 0)
 })
